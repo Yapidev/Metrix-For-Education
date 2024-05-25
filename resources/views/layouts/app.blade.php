e6ab74b0fa5b3e30619c63bfac817ed53575eb38
Yapidev <88489723+Yapidev@users.noreply.github.com>
2024-05-25 11:56:04 +0700

    seo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,23 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- Title --}}
-    <title>Metrix For Education</title>
+    <title>Metrix For Education - Smart People Behind Smartphone</title>
+
+    {{-- Keyword and Description --}}
+    <meta name="keywords"
+        content="Metrix For Education, metrixflasher, metrixforeducation, metrix education ,pendidikan online, e-learning, sumber daya pendidikan, pembelajaran cerdas, pembelajaran melalui smartphone, pendidikan berkualitas, kursus online, alat pendidikan">
+    <meta name="description"
+        content="Sebagai lembaga pelatihan teknisi smartphone berpengalaman
+        sejak tahun 2000, kami telah berhasil meluluskan lebih dari 3000 teknisi, di mana hampir
+        90% dari mereka telah membuka usaha sendiri. Pendekatan pengajaran kami sangat berbeda,
+        karena kami tidak hanya fokus pada hafalan, tetapi kami memberikan pengajaran yang menyeluruh
+        mulai dari teori dasar hingga teknik perbaikan tingkat mahir. Metode pembelajaran yang kami
+        terapkan sangat menarik dan tidak membosankan, sehingga memicu peserta untuk berpikir secara
+        kritis dan kreatif. Kami bangga menjadi lembaga yang mendorong peserta kami mencapai pemahaman
+        yang mendalam dan membantu mereka dalam mengembangkan keterampilan teknis yang diperlukan.
+        Dengan dedikasi kami dalam memberikan kualitas pelatihan yang terbaik, kami siap membantu
+        peserta kami meraih kesuksesan dan mencapai tujuan mereka dalam bidang teknisi smartphone.">
+    {{-- Keyword and Description --}}
 
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
@@ -30,15 +46,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="{{asset('lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     {{-- STACK STYLE --}}
     @stack('style')

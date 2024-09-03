50ce14fcc9dfe0116ab63a6ee83641a4df59c125
hambarthegoat <www.ilhamakbar3@gmail.com>
2024-09-03 21:28:22 +0700

    Tambahan link bergabung

--- resources/views/components/bergabung.blade.php
+++ resources/views/components/bergabung.blade.php
@@ -1,7 +1,7 @@
 <!-- Bergabung Start -->
 <div class="container-fluid text-center bergabung px-4 px-lg-5 bg-secondary" style="padding: 50px 0;">
     <h2 class="title-section mb-4" style="margin-bottom: 20px;">Tertarik bergabung bersama kami?</h2>
-    <a href="#" class="btn btn-primary btn-lg" style="padding: 10px 30px; font-size: 18px; border-radius: 50px; background-color: #ffffff; color: #6c757d; border: 2px solid #6c757d;"
+    <a href="https://forms.gle/6BXEiM37GFjtc7cJ9" target="_blank" rel="noopener noreferrer" class="btn btn-primary btn-lg" style="padding: 10px 30px; font-size: 18px; border-radius: 50px; background-color: #ffffff; color: #6c757d; border: 2px solid #6c757d;"
    onmouseover="this.style.backgroundColor='#28a745'; this.style.color='#ffffff'; this.style.borderColor='#28a745';"
    onmouseout="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.borderColor='#6c757d';">
    Bergabung
